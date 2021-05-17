cf47f11824f1502dcf1bc5cb06b662e0f4548394
SadriG91 <sadri.gargouri@teliacompany.com>
2021-05-17 15:30:06 +0200

    updated aws provider

--- examples/basic/versions.tf
+++ examples/basic/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.61"
+      version = "~> 3.0"
     }
   }
   required_version = ">= 0.14"
--- versions.tf
+++ versions.tf
@@ -2,7 +2,6 @@ terraform {
   required_version = ">= 0.14"
 
   required_providers {
-    # 3.0 makes aws_s3_bucket.region read-only which is a breaking change for this module.
-    aws = "~> 2.0"
+    aws = "~> 3.0"
   }
 }
